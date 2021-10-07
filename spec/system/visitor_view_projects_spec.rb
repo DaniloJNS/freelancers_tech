8e720cba9c549f6863a5e56398f523eb633870b0
DaniloJNS <dan.silva.13.zx@gmail.com>
2021-10-07 17:05:09 -0300

    add user and your auth

--- spec/system/visitor_view_projects_spec.rb
+++ spec/system/visitor_view_projects_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 describe 'visitor view projects' do
+  it 'using menu' do
+    visit root_path
 
+    expect(page).to have_link('Ver Projetos', href: projects_path)
+  end
   it 'succefully' do
-    Project.create!(title: 'Ecommerce de carros', description: 'uma plataforma para venda, troca e comparar de carros', deadline_submission: '11/12/2021')
+    Project.create!(title: 'Ecommerce de carros', description: 'uma plataforma para venda, '\
+                    'troca e compra de carros', deadline_submission: '21/12/2021')
     visit root_path
     click_on "Ver Projetos"
 
     expect(page).to have_content("Ecommerce de carros")
-    expect(page).to have_content("uma plataforma para venda, troca e comparar de carros")
+    expect(page).to have_content("uma plataforma para venda, troca e compra de carros")
+    expect(page).to have_content("2021-12-21") 
+  end
+
+  it 'and theres no projects available' do
+    visit root_path
+
+    click_on "Ver Projetos"
+
+    expect(page).to have_content("Ops, no momento não temos projetos abertos") 
+    expect(page).to have_content("Tente novamente mais tarde") 
   end
 end
